f5e3745c690a98276eb500980d72f968707d688b
Anton Kuznetsov <isnifer@gmail.com>
2013-07-22 00:37:47 +0400

    Make all methods

--- task.js
+++ task.js
@@ -5,7 +5,14 @@
  * @param {Number}[] position Местоположение корабля.
  * @param {Number} capacity Грузоподъемность корабля.
  */
-function Vessel(name, position, capacity) {}
+function Vessel(name, position, capacity) {
+
+  this.name = name;
+  this.position = position;
+  this.capacity = capacity;
+  this.load = 0;
+
+}
 
 /**
  * Выводит текущее состояние корабля: имя, местоположение, доступную грузоподъемность.
@@ -15,19 +22,31 @@ function Vessel(name, position, capacity) {}
  * vesserl.report(); // Грузовой корабль. Местоположение: 50,20. Груз: 200т.
  * @name Vessel.report
  */
-Vessel.prototype.report = function () {}
+Vessel.prototype.report = function () {
+
+  return "Корабль: " + this.name + ". Местоположение: " + this.position[0] + ', ' + this.position[1] + '. Занято: ' + this.getOccupiedSpace() + " из " + this.capacity + "т";
+
+}
 
 /**
  * Выводит количество свободного места на корабле.
  * @name Vessel.getFreeSpace
  */
-Vessel.prototype.getFreeSpace = function () {}
+Vessel.prototype.getFreeSpace = function () {
+
+  return this.capacity - this.load;
+
+}
 
 /**
  * Выводит количество занятого места на корабле.
  * @name Vessel.getOccupiedSpace
  */
-Vessel.prototype.getOccupiedSpace = function () {}
+Vessel.prototype.getOccupiedSpace = function () {
+
+  return this.load;
+
+}
 
 /**
  * Переносит корабль в указанную точку.
@@ -39,7 +58,17 @@ Vessel.prototype.getOccupiedSpace = function () {}
  * vessel.flyTo(earth);
  * @name Vessel.report
  */
-Vessel.prototype.flyTo = function (newPosition) {}
+Vessel.prototype.flyTo = function (newPosition) {
+  
+  if (newPosition.length === 2) {
+    this.position = newPosition;
+  } else if (typeof newPosition === 'object' && typeof newPosition.position !== undefined) {
+    this.position = newPosition.position;
+  } else {
+    return 'Введите верные координаты';
+  }
+
+}
 
 /**
  * Создает экземпляр планеты.
@@ -48,19 +77,33 @@ Vessel.prototype.flyTo = function (newPosition) {}
  * @param {Number}[] position Местоположение планеты.
  * @param {Number} availableAmountOfCargo Доступное количество груза.
  */
-function Planet(name, position, availableAmountOfCargo) {}
+function Planet(name, position, availableAmountOfCargo) {
+
+  this.name = name;
+  this.position = position;
+  this.avaiableCargo = availableAmountOfCargo;
+
+}
 
 /**
  * Выводит текущее состояние планеты: имя, местоположение, количество доступного груза.
  * @name Planet.report
  */
-Planet.prototype.report = function () {}
+Planet.prototype.report = function () {
+
+  return "Планета: " + this.name + ". Местоположение: " + this.position[0] + ', ' + this.position[1] + '. Доступно груза: ' + this.getAvailableAmountOfCargo() + "т";
+
+}
 
 /**
  * Возвращает доступное количество груза планеты.
  * @name Vessel.getAvailableAmountOfCargo
  */
-Planet.prototype.getAvailableAmountOfCargo = function () {}
+Planet.prototype.getAvailableAmountOfCargo = function () {
+
+  return this.avaiableCargo;
+
+}
 
 /**
  * Загружает на корабль заданное количество груза.
@@ -70,7 +113,17 @@ Planet.prototype.getAvailableAmountOfCargo = function () {}
  * @param {Number} cargoWeight Вес загружаемого груза.
  * @name Vessel.loadCargoTo
  */
-Planet.prototype.loadCargoTo = function (vessel, cargoWeight) {}
+Planet.prototype.loadCargoTo = function (vessel, cargoWeight) {
+
+  if (vessel.position[0] === this.position[0] && vessel.position[1] === this.position[1]) {
+    var freeSpace = vessel.getFreeSpace();
+    if (freeSpace >= cargoWeight) {
+      vessel.load += cargoWeight;
+      this.avaiableCargo -= cargoWeight;
+    }
+  }
+
+}
 
 /**
  * Выгружает с корабля заданное количество груза.
@@ -80,4 +133,12 @@ Planet.prototype.loadCargoTo = function (vessel, cargoWeight) {}
  * @param {Number} cargoWeight Вес выгружаемого груза.
  * @name Vessel.unloadCargoFrom
  */
-Planet.prototype.unloadCargoFrom = function (vessel, cargoWeight) {}
+Planet.prototype.unloadCargoFrom = function (vessel, cargoWeight) {
+
+  if (vessel.position[0] === this.position[0] && vessel.position[1] === this.position[1]) {
+    var load = vessel.getOccupiedSpace();
+    vessel.load -= cargoWeight;
+    this.avaiableCargo += cargoWeight;
+  }
+
+}
